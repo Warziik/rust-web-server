84b1cef291f3600ca8d280d42ce0eeacf872959a
Alex Chesnay <alex.chesnay@eyeneed.fr>
2023-10-01 23:52:09 +0200

    feat: create very simple web server handling one url and responding with one file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::read_to_string;
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let stream = TcpListener::bind("127.0.0.1:3000").unwrap();
+    for s in stream.incoming() {
+        let stream = s.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let status_line = String::from("HTTP/1.1 200 OK");
+    let contents = read_to_string("public/index.html").unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    println!("Request handling");
+    stream.write_all(response.as_bytes()).unwrap();
 }
