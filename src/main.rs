9bbe17ebd08817b937f5f54d13834b07b2521abc
Alex Chesnay <alex.chesnay@eyeneed.fr>
2023-10-09 23:30:15 +0200

    feat: transform single-threaded web server into a multi-threaded web server

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
 use std::fs::read_to_string;
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
+use web_server::ThreadPool;
 
 fn main() {
     let stream = TcpListener::bind("127.0.0.1:3000").unwrap();
+    let pool = ThreadPool::new(4);
     for s in stream.incoming() {
         let stream = s.unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let status_line = String::from("HTTP/1.1 200 OK");
-    let contents = read_to_string("public/index.html").unwrap();
-    let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     println!("Request handling");
+    let reader = BufReader::new(&mut stream);
+
+    let request_line = reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_millis(5000));
+            ("HTTP/1.1 200 OK", "index.html")
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = read_to_string(format!("public/{filename}")).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
 }
